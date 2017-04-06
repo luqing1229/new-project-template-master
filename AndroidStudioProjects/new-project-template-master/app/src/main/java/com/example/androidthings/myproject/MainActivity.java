d145e4b5cb1c430f01156d15f85cf364048a653e
luqing <cmcc@cmcc-PC>
2017-04-06 19:27:43 +0800

    android things测试

--- AndroidStudioProjects/new-project-template-master/app/src/main/java/com/example/androidthings/myproject/MainActivity.java
+++ AndroidStudioProjects/new-project-template-master/app/src/main/java/com/example/androidthings/myproject/MainActivity.java
@@ -52,6 +52,8 @@ public class MainActivity extends Activity {
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy");
+        Log.d(TAG, "onDestroy");
+
 
     }
 }
